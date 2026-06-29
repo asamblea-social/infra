e0f34bf6cf60ae56cfbcb9bdb6529b3e02340cb4
asamblea-social <admin@asamblea.social>
2026-06-29 06:55:38 +0000

    mumble-web: respaldar dist funcional con MetroMumbleDark y webpack.config.js corregido

---
+++ mumble-web/dist/config.local.js
@@ -0,0 +1,5 @@
+let config = window.mumbleWebConfig
+config.defaults.address = 'mumble.asamblea.social'
+config.defaults.port = '443'
+config.defaults.theme = 'MetroMumbleDark'
+config.defaults.webrtc = 'false'
