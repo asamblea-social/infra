899f24734b5616b2efda296a06f642c793c20728
asamblea-social <admin@asamblea.social>
2026-06-29 07:05:30 +0000

    mumble-web: config.local.js final - ocultar dirección, puerto y tokens

--- mumble-web/config.local.js
+++ mumble-web/config.local.js
@@ -3,3 +3,6 @@ config.defaults.address = 'mumble.asamblea.social'
 config.defaults.port = '443'
 config.defaults.theme = 'MetroMumbleDark'
 config.defaults.webrtc = 'false'
+config.connectDialog.address = false
+config.connectDialog.port = false
+config.connectDialog.token = false
